fe171ac5766b5635d3b0b2398a47bbef248c9285
niranjan-aware <niroba.aware.26@gmail.com>
2025-06-04 14:37:08 +0530

    basic auth api done, logout remaining with jwt

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,81 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      
+      res.status(200).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        createdAt: newUser.createdAt,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.createdAt,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const logout = (req, res) => {
+  try {
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js"
+
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
